7a499b47e76d2c162d2e0fe848464e8c0cdd422f
PetrescuRobert <petrescu.robert69@gmail.com>
2025-11-20 18:44:14 +0200

    Jobs microservice + config module

--- apps/jobber-jobs/src/main.ts
+++ apps/jobber-jobs/src/main.ts
@@ -3,15 +3,30 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Enable global pipes:
+   * - Validation for incoming requests (whitelist: true ->
+   * validator will strip validated object of any properties that do not have any decorators)
+   */
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  /**
+   * Enable cookie parser middleware
+   */
+  app.use(cookieParser());
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = app.get(ConfigService).getOrThrow('PORT');
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
