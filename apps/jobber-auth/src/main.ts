950260df62f5f50dcf9cfdfeb9f365f5d2787564
PetrescuRobert <petrescu.robert69@gmail.com>
2025-11-24 20:30:56 +0200

    grpc setup

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -9,6 +9,10 @@ import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 
+import { AUTH_PACKAGE_NAME } from '@jobber/grpc';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -27,6 +31,19 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = app.get(ConfigService).getOrThrow('PORT');
+
+  /**
+   * Enable grpc
+   */
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+  await app.startAllMicroservices();
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
